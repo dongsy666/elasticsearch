ea515a8e7027140759c2ccceb47f244308f1a0b5
springboot <dongsy666>
2020-05-13 16:21:55 +0800

    完结

--- es-jd/src/main/java/com/dsy/EsJdApplication.java
+++ es-jd/src/main/java/com/dsy/EsJdApplication.java
@@ -2,7 +2,12 @@ package com.dsy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
+/**
+ * @author trueway
+ */
 @SpringBootApplication
 public class EsJdApplication {
 
--- es-jd/src/main/java/com/dsy/utils/HtmlParseUtils.java
+++ es-jd/src/main/java/com/dsy/utils/HtmlParseUtils.java
@@ -1,26 +1,30 @@
 package com.dsy.utils;
 
+import com.dsy.pojo.Content;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author trueway
  */
+@Component
 public class HtmlParseUtils {
-    public static void main(String[] args) throws IOException {
+ /*   public static void main(String[] args) throws IOException {
+        new HtmlParseUtils().parseJD("vue").forEach(System.out::println);
+    }*/
 
-    }
-
-    public List<Object> parseJD(String keyword)throws IOException {
+    public List<Content> parseJD(String keyword)throws IOException {
         //获取请求  https://search.jd.com/Search?keyword=java
         //前提,需要联网,    ajax不能获取到,需要模拟浏览器才能获取到
-        String url="https://search.jd.com/Search?keyword=java";
+        String url="https://search.jd.com/Search?keyword="+keyword;
         try {
             SslUtils.ignoreSsl();
         } catch (Exception e) {
@@ -33,18 +37,20 @@ public class HtmlParseUtils {
         //获取所有的li元素
         Elements elements = element.getElementsByTag("li");
         //获取元素中的内容,这里el就是每个li标签了
+        ArrayList<Content> goodsList = new ArrayList<>();
         for (Element el : elements) {
             //关于这种图片特别多的网站,所有图片都是延迟加载的
             //source-data-lazy-img
             String img = el.getElementsByTag("img").eq(0).attr("src");
             String price = el.getElementsByClass("p-price").eq(0).text();
             String title = el.getElementsByClass("p-name").eq(0).text();
-            System.out.println("=================");
-            System.out.println(img);
-            System.out.println(price);
-            System.out.println(title);
+            Content content = new Content();
+            content.setImg(img);
+            content.setTitle(title);
+            content.setPrice(price);
+            goodsList.add(content);
         }
-    return null;
+    return goodsList;
     }
 
 }
